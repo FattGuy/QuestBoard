badaf1497014716f1210a4a20d146a6cf1ec2c44
FattGuy <chang809@umn.edu>
2016-03-19 15:43:19 -0500

    Initial Commit

---
+++ QuestBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuestBoard
+//
+//  Created by Feng Chang on 3/19/16.
+//  Copyright © 2016 FattGuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuestBoard/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  QuestBoard
+//
+//  Created by Feng Chang on 3/19/16.
+//  Copyright © 2016 FattGuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
